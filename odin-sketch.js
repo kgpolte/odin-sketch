6876d3425b4b1fecef3c3c69747eed3c455cb4d2
Kenny Polte <120602839+kgpolte@users.noreply.github.com>
2023-01-07 11:08:52 -0800

    Add basic drawing functionality

--- odin-sketch.js
+++ odin-sketch.js
@@ -1,8 +1,14 @@
 function buildGrid(squaresPerSide = 24){
 
+    if (squaresPerSide > 50) {
+        squaresPerSide = 50;
+    } else if (squaresPerSide < 1) {
+        squaresPerSide = 1;
+    }
+
     const gridSize = 720;
     const grid = document.querySelector('.grid');
-    
+
     grid.style.height = `${gridSize}px`;
     grid.style.width = `${gridSize}px`;
     grid.style.gridTemplateColumns = `repeat(${squaresPerSide}, 1fr)`;
@@ -12,6 +18,9 @@ function buildGrid(squaresPerSide = 24){
         const gridItem = document.createElement('div');
         gridItem.classList.add('cell');
         grid.appendChild(gridItem);
+
+        gridItem.addEventListener('mouseover', e => gridItem.style.background = '#000000');
+
     }
 }
 
