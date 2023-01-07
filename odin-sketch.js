176147fc2286b6a2feae469e06d82580c7621e35
Kenny Polte <120602839+kgpolte@users.noreply.github.com>
2023-01-07 12:06:14 -0800

    Edit layout and styles

--- odin-sketch.js
+++ odin-sketch.js
@@ -1,19 +1,23 @@
-function buildGrid(squaresPerSide = 24){
+function buildGrid(resolution = 24){
 
-    if (squaresPerSide > 50) {
-        squaresPerSide = 50;
-    } else if (squaresPerSide < 1) {
-        squaresPerSide = 1;
+    // Handle invalid inputs
+    if (resolution > 50) {
+        resolution = 50;
+    } else if (resolution < 1) {
+        resolution = 1;
+    } else if (typeof resolution != 'number') {
+        resolution = 24;
     }
 
-    const gridSize = 720;
+    // Set the size of the grid container and define the style rules
+    const gridSize = 600;
     const grid = document.querySelector('.grid');
-
     grid.style.height = `${gridSize}px`;
     grid.style.width = `${gridSize}px`;
-    grid.style.gridTemplateColumns = `repeat(${squaresPerSide}, 1fr)`;
+    grid.style.gridTemplateColumns = `repeat(${resolution}, 1fr)`;
 
-    for (let i = 0; i < squaresPerSide ** 2; i++) {
+    // Create grid elements in the DOM
+    for (let i = 0; i < resolution ** 2; i++) {
 
         const gridItem = document.createElement('div');
         gridItem.classList.add('cell');
@@ -22,6 +26,10 @@ function buildGrid(squaresPerSide = 24){
         gridItem.addEventListener('mouseover', e => gridItem.style.background = '#000000');
 
     }
+
+    // Set the default value of the resolution input field
+    const resInput = document.getElementById('resolution');
+    resInput.value = resolution;
 }
 
 buildGrid();
