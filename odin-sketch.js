d7fd2a7f948d6d505abff8ff0136167dd5a6a863
Kenny Polte <120602839+kgpolte@users.noreply.github.com>
2023-01-07 09:14:55 -0800

    Write function to generate the tile grid

---
+++ odin-sketch.js
@@ -0,0 +1,19 @@
+function buildGrid(gridSize = 16){
+
+    const grid = document.querySelector('.grid');
+
+    for (let i = 0; i < gridSize; i++) {
+
+        const row = document.createElement('div');
+        row.classList.add('row');
+
+        for (let j = 0; j < gridSize; j++) {
+
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            row.appendChild(cell);
+        }
+
+        grid.appendChild(row);
+    }
+}
